771a103887264e076cb398afa127e560818a365d
Christopher Robin <williamchen1506@gmail.com>
2025-04-27 01:08:50 +0700

    `refactor:` clean code

--- internal/middleware/rate_limiter.go
+++ internal/middleware/rate_limiter.go
@@ -16,7 +16,6 @@ type TokenBucket struct {
 	lastRefillTime time.Time
 }
 
-// NewTokenBucket creates a new TokenBucket instance.
 func NewTokenBucket(maxTokens float64, refillRate float64) *TokenBucket {
 	return &TokenBucket{
 		tokens:         maxTokens,
@@ -26,7 +25,6 @@ func NewTokenBucket(maxTokens float64, refillRate float64) *TokenBucket {
 	}
 }
 
-// refill refills the token bucket based on the elapsed time since the last refill.
 func (tb *TokenBucket) refill() {
 	now := time.Now()
 	duration := now.Sub(tb.lastRefillTime)
@@ -35,8 +33,7 @@ func (tb *TokenBucket) refill() {
 	tb.lastRefillTime = now
 }
 
-// Request checks if the token bucket has enough tokens for a request.
-// It deducts the tokens and returns true if successful, otherwise false.
+
 func (tb *TokenBucket) Request(tokens float64) bool {
 	tb.mu.Lock()
 	defer tb.mu.Unlock()
@@ -49,15 +46,12 @@ func (tb *TokenBucket) Request(tokens float64) bool {
 	return false
 }
 
-// Global TokenBucket for the whole server
+
 var globalTokenBucket = NewTokenBucket(1000, 10) 
 
-// TokenBucketMiddleware is a middleware for global rate limiting
 func RateLimiter() fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		// Check if the request can proceed (1 token per request)
 		if !globalTokenBucket.Request(1) {
-			// If not enough tokens, return HTTP 429 Too Many Requests
 			return c.Status(fiber.StatusTooManyRequests).SendString("Too many requests")
 		}
 
